eb37bc0f97836d025cfebc5a6a5e33645039f3a6
stbrown <shadowtuner1@gmail.com>
2018-09-21 20:01:23 -0500

    Finished setting up connection string.

--- SQLProject/LocationRepository.cs
+++ SQLProject/LocationRepository.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
 namespace SQLProject
 {
     public class LocationRepository
     {
+        private static string connectionString;
 
+        public LocationRepository(string _connectionString)
+        {
+            connectionString = _connectionString;
+        }
+
+        public List<Location> GetLocations()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+            }
+        }
     }
 }
